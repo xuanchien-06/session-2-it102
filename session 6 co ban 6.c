9b44e8bc72b715d5405d780c2bedba446aa2bded
Tạ Xuân Chiến; HN-JPN-KS24C; lập trình scratch; session 3 <xuanchien319@gmail.com>
2024-11-20 22:35:23 +0700

    session 2 cơ bản 6

---
+++ session 6 co ban 6.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(){
+	const float PI = 3.14;
+	int R = 4;
+	int C;
+	int S;
+	C = (2 * R) * PI;
+	printf("chu vi cua hinh tron là %d \n",C);
+	S = (R * R) * PI;
+	printf("dien tich cua hinh tron la %d",S);
+	
+	
+	return 0;
+}
