b2cc8bed6787938aab7c535976f734d752b8a81d
Tạ Xuân Chiến; HN-JPN-KS24C; lập trình scratch; session 3 <xuanchien319@gmail.com>
2024-11-20 22:06:43 +0700

    session 2 cơ bản 4

---
+++ session 2 co ban 4.cpp
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(){
+	//canh hinh vuong = a
+	int a = 5;
+	int p;
+	int s;
+	//chu vi hinh vuong = p
+	p = a * 4;
+	printf("chu vi hinh vuong la %d \n",p);
+	//dien tich hinh vuong
+	s = a * a;
+	printf("dien tich hinh vuong la %d \n",s);
+	return 0;
+}
