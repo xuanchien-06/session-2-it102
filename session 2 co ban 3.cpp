39e5698587d6a96d3d3ae72cc26c654c10a064d4
Tạ Xuân Chiến; HN-JPN-KS24C; lập trình scratch; session 3 <xuanchien319@gmail.com>
2024-11-20 22:06:20 +0700

    sesion 2 cơ bản 3

---
+++ session 2 co ban 3.cpp
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main(){
+	int a = 20;
+	int b = 10;
+	int sum, hieu , tich , thuong;
+	sum = a + b;
+	printf("tong hai so la %d \n",sum);
+	hieu = a - b;
+	printf ("hieu hai so la %d \n",hieu);
+	tich = a * b;
+	printf("tich cua hai so la %d \n",tich);
+	thuong = a / b;
+	printf("thuong cua hai so la %d \n",thuong);
+
+	
+	return 0;
+}
