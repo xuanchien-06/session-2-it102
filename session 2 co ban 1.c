85720e8f67037d607efcae0ec71ce07d2f4ef8c3
Tạ Xuân Chiến; HN-JPN-KS24C; lập trình scratch; session 3 <xuanchien319@gmail.com>
2024-11-20 22:04:50 +0700

    sesison 2 cơ bản 1

---
+++ session 2 co ban 1.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int main(){
+	//khai bao bien kieu du lieu int
+	int age;
+	//khai bao bien kieu du lieu int va gan gia tri
+	int year = 2024;
+	// truy xuat lay gia tri cua bien
+	printf("nam nay la %d", year);
+
+	//thay doi gia tri cua bien age
+	age = 18;
+	printf("\n nam nay toi %d tuoi", age);
+
+	//khai bao bien co kieu du lieu ky tu
+	char kytu = 'a';
+	printf("\n ky tu la %c",kytu);
+	
+	//khai bao bien kieu so thuc
+	float pi = 3.14;
+	printf("\n so pi la %.2f",pi);
+	return 0;
+	}
