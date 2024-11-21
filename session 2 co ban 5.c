7fdad2de493f0be0f4f5dc26571a6553d28dfae0
Tạ Xuân Chiến; HN-JPN-KS24C; lập trình scratch; session 3 <xuanchien319@gmail.com>
2024-11-21 08:48:09 +0700

    session 2 cơ bản 5

---
+++ session 2 co ban 5.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(){
+	int a = 5;
+	int b = 10;
+	int p;
+	int s;
+	p = 2 * (a + b);
+	printf("chu vi hinh chu nhat la %d \n",p);
+	s = a * b;
+	printf("dien tich hinh chu nhat la %d",s);
+	return 0;
+}
